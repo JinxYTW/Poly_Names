cf53a9a648fac1719fc5d5902cc39f3781b61c76
JinxYTW <tyohann974@gmail.com>
2024-06-13 14:15:30 +0200

    Finalisation API Indice

--- back/src/dao/TourDao.java
+++ back/src/dao/TourDao.java
@@ -63,13 +63,15 @@ public class TourDao {
             PartieDao myPartieDao = new PartieDao();
             Partie myPartie = myPartieDao.findByCode(uniqueCode);
             int idPartie = myPartie.id_partie();
+            int word_to_guess= wordToFindNb + 1;
 
             PolyNameDatabase myDatabase = new PolyNameDatabase();
-            String request = "INSERT INTO tour (indice, word_to_find_nb, id_partie) VALUES (?, ?, ?)";
+            String request = "INSERT INTO tour (indice, word_to_find_nb,word_to_guess, id_partie) VALUES (?,?, ?, ?)";
             PreparedStatement prepStat = myDatabase.prepareStatement(request);
             prepStat.setString(1, indice);
             prepStat.setInt(2, wordToFindNb);
-            prepStat.setInt(3, idPartie);
+            prepStat.setInt(3, word_to_guess);
+            prepStat.setInt(4, idPartie);
             prepStat.executeUpdate();
         } catch (Exception e) {
             System.out.println(e.getMessage());
